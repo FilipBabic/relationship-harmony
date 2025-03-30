eb4b60c4513a4b384fcb5e1f7f83c9daa25582fc
Filip Babic <filip1986babic@gmail.com>
2025-03-31 01:19:41 +0200

    ESLint&Prettier Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 const Home = () => {
-  return <h1 className="text-center text-2xl text-purple-300">Welcome to RELATIONSHIP HARMONY</h1>;
+  return (
+    <h1 className="text-center text-2xl text-purple-300 ">Welcome to RELATIONSHIP HARMON Y</h1>
+  );
 };
 
 export default Home;
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,67 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import tailwindcss from "eslint-plugin-tailwindcss";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+export default [
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "plugin:tailwindcss/recommended",
+    "prettier"
+  ),
 
-export default eslintConfig;
+  {
+    plugins: {
+      tailwindcss,
+    },
+    rules: {
+      ...tailwindcss.configs.recommended.rules,
+      "tailwindcss/classnames-order": "warn", // Ensures Tailwind classes are sorted
+      "tailwindcss/enforces-shorthand": "warn", // Suggests shorthand class names
+      "tailwindcss/no-custom-classname": "off", // Turn off if using custom class names
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin", // Built-in types are first
+            "external", // External libraries
+            "internal", // Internal modules
+            ["parent", "sibling"], // Parent and sibling types can be mingled together
+            "index", // Then the index file
+            "object", // Object imports
+          ],
+          "newlines-between": "always",
+          pathGroups: [
+            {
+              pattern: "@app/**",
+              group: "external",
+              position: "after",
+            },
+          ],
+          pathGroupsExcludedImportTypes: ["builtin"],
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+      "import/no-anonymous-default-export": "off",
+    },
+  },
+  {
+    files: ["components/cards/QuestionCard.tsx"],
+    rules: {
+      "no-undef": "off",
+    },
+  },
+];
